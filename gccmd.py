ac6f5c189ee0e9b4040a34d8b209909a907ebbce
DaKATFailsImmenselyAtCoding <82332093+DaKATFailsImmenselyAtCoding@users.noreply.github.com>
2021-04-28 19:44:47 +0200

    Added ~look and ~transform

--- gccmd.py
+++ gccmd.py
@@ -22,17 +22,22 @@ register_txt = 'register'
 menu_txt = 'menu'
 work_txt = 'work'
 goto_txt = 'goto'
+look_txt = 'look'
 order_txt = 'order'
+transform_txt = 'transform'
+transform_txt_alt1 = 'tfm'
 db_txt = 'database'
 map_txt = 'map'
 
 #this part assigns locations with their chanenl ID
+downtown_channel_id = 798059782871056384
 study_hall_channel_id = 788095887884288052
 moonlight_cafe_channel_id = 805464853967929344
 mall_channel_id = 798059805172170782
 
 #command responses
-study_response = "You work on your homework while vibing to some nice LoFi beats!"
+study_response = "You work on your homework while vibing to some nice LoFi beats!" #random.randrange (50) [nono]
+    #if study_response >=0 and study_response < 10: #20% chance, same for the rest [nope]  
 bake_response = 'you take a break to bake a nice batch of cookies.'
 allow_response = 'What a good girl! here, have some allowance.'
 
@@ -137,6 +142,19 @@ async def goto_cmd(msg):
         print('added' + str(role))
 
 '''
+    Allows people to look where they are
+'''
+async def look_cmd(msg):
+    if: msg.channel.id == downtown_channel_id:
+            response = "You are in the city center, the heart of Phonosia. With daylight its full of salary men on their way to the office to do 'serious business' and scholars going to school (duh). But on the nighttime, the clubs and discos in the area open to their drunken regulars, lighting with neon dye the city's skyscrapers. Lately a rumour has arisen saying that here, at night, avoiding the young ravers and the old alcoholics, a fight between the forces of good and bad takes place, where monsters are fought by unknown yet powerful individuals. Of course, the city's authorities have qualified this rumour of 'just another urban legend'."
+    if: msg.channel.id == study_hall_channel_id:
+            response = "You find yourself in a modern, white, well-lighted library with massive windows that show a precious city view. Here all the students in the city amass books and knowledge to pass their exams and reach their dreams! The massive shelves that reach this place's ceiling are full of all kinds of books: big books, thin books, old books, new books... This is definitely a good place to **~study** in, if you focus and keep yourself quiet!"
+    if: msg.channel.id == moonlight_cafe_channel_id:
+            response = "You find yourself in a small and clean cafe with wooden furniture and some comfy sofas. This hipster-looking cafeteria was recently bought by a young long-bearded guy with prominent arm tattoos who's highest aspiration in life was to run his own coffee shop. The walls have some posters of old movies you haven't seen but you guess they're good, because if they weren't the man running this place wouldn't have put them there. On one side of the room, near the sofas, there are two small shelves: One containing old vinyl records that are the background music of the place, and another one containing 80s Japanese manga that the costumers can read while eating. They're mostly mahō shōjo series. Weirdly specific, you think."
+    if: msg.channel.id == mall_channel_id:
+            response = "You are in the middle of a gigantic masterpiece of design and architecture from the construction boom that happened forty years ago. The flashing lights of all the ads for amazing products, the fancy-looking window displays, the sweet melodies that go from the speakers to your ears and the always moving people mass are turning your brain into the always-consuming machine is supposed to be, making you feel slightly dizzy. Down in the hallways you find your local gig where you can **~work** in. You will have to move with your elbows through the shopper stampede to arrive there, though."
+      
+'''
     Pays user per command, if done in mall
 '''
 async def work_cmd(msg):
@@ -147,6 +165,15 @@ async def work_cmd(msg):
         gcdb.setPlayerAttribute(msg.author.id, 'money', total)
         await msg.channel.send('*' + str(msg.author.display_name) + ':* ' + response)
 
+'''
+    Allows the user to see their transformation into a magical girl
+'''
+async def transform_cmd(msg): #In the future I'll turn it into a tag, for now it is just hard to unlock flavor text
+    if: currentLofi >= 1000000
+        response = "Epic orchestral music starts to sound. You summon the power of friendship and love and scream your magical girl name with all your soul. Your regular student clothes begin to disappear to reveal a super cute outfit without ever showing anything NSFW to the camera. People around you are hypnotized by this transformation, you notice that thanks to the 'Damns' and 'Holy fucks' they let out. You are a magical girl now, go kick some bad guy's ass!"  
+    else:
+        response = "You focus but when you try transforming yourself into the magical girl you truly are, you faint for a second. You will need some more lofi to do this! ({}/1000000 lofi)".format(gcdb.getPlayerAttribute(msg.author.id, 'lofi'))
+        
 '''
     Setup the user's database entry if they have none
 '''
@@ -177,9 +204,12 @@ cmd_dict = {
     money_txt : money_cmd,
     work_txt : work_cmd,
     goto_txt : goto_cmd,
+    look_txt : look_cmd,
     order_txt : order_cmd,
     menu_txt : menu_cmd,
     register_txt : register_cmd,
+    transform_txt : transform_cmd,
+    transform_txt_alt1 : transform_cmd,
     db_txt : db_cmd,
     map_txt : map_cmd,
 }
